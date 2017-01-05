848260fece5056f4b2f496a8fecbad1038a7443a
Bobby Earl <bobby.earl@blackbaud.com>
2017-01-05 10:23:17 -0500

    Adding header

--- after-success.sh
+++ after-success.sh
@@ -1,6 +1,16 @@
 # Fail the build if this step fails
 set -e
 
+say "
+ 888888ba  dP                   dP       dP                               dP 
+ 88    `8b 88                   88       88                               88 
+a88aaaa8P' 88 .d8888b. .d8888b. 88  .dP  88d888b. .d8888b. dP    dP .d888b88 
+ 88   `8b. 88 88'  `88 88'  `"" 88888"   88'  `88 88'  `88 88    88 88'  `88 
+ 88    .88 88 88.  .88 88.  ... 88  `8b. 88.  .88 88.  .88 88.  .88 88.  .88 
+ 88888888P dP `88888P8 `88888P' dP   `YP 88Y8888' `88888P8 `88888P' `88888P8 
+ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+"
+
 # Necessary to stop pull requests from forks from running outside of Savage
 # Publish a tag to NPM
 if [[ "$TRAVIS_SECURE_ENV_VARS" == "true" && -n "$TRAVIS_TAG" ]]; then
