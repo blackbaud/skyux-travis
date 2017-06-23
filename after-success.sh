fc8e9aa22525fcea9eb556f627eef97e70ed9a99
Bobby Earl <bobby.earl@blackbaud.com>
2017-06-22 20:48:58 -0400

    Update script to use auth token if available.

--- after-success.sh
+++ after-success.sh
@@ -3,16 +3,34 @@ set -e
 
 echo -e "Blackbaud - SKY UX Travis - After Success"
 
+function publish {
+  echo -e "Publishing to NPM..."
+  npm publish --access public
+  echo -e "Successfully published to NPM.\n"
+}
+
 # Necessary to stop pull requests from forks from running outside of Savage
 # Publish a tag to NPM
 if [[ "$TRAVIS_SECURE_ENV_VARS" == "true" && -n "$TRAVIS_TAG" ]]; then
-  echo -e "Logging in to NPM..."
-  echo -e "blackbaud\n$NPM_PASSWORD\nsky-savage@blackbaud.com" | npm login
-  echo -e "Publishing to NPM..."
-  npm publish --access public
-  echo -e "Logging out of NPM..."
-  npm logout
-  echo -e "Successfully published to NPM.\n"
+  if [[ $NPM_TOKEN ]]; then
+
+    echo -e "Logging in via NPM_TOKEN"
+    echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
+    publish
+    echo -e "Logging out via NPM_TOKEN"
+    rm .npmrc
+
+  elif [[ $NPM_PASSWORD ]]; then
+
+    echo -e "Logging in via NPM_PASSWORD"
+    echo -e "blackbaud\n$NPM_PASSWORD\nsky-savage@blackbaud.com" | npm login
+    publish
+    echo -e "Logging out via NPM_PASSWORD"
+    npm logout
+
+  else
+    echo -e "Unable to publish to NPM as no credentials are not available"
+  fi
 else
-  echo -c "Ignoring Script"
+  echo -e "Ignoring Script"
 fi
