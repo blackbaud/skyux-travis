fb6ae239533e5620062fac807b36d4a727a0a418
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-09-05 09:51:09 -0400

    Added scripts for v.3.0.0 libraries

---
+++ library-install-v3.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+# Necessary to stop pull requests from forks from running
+if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
+  npm install
+  npm install --no-save blackbaud/skyux-builder-config#visual-scripts
+else
+  echo -e "Pull requests from forks are run via Savage."
+fi
---
+++ library-script-v3.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+# Necessary to stop pull requests from forks from running
+if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
+  npm run test:ci
+  npm run test:visual:ci
+else
+  echo -e "Pull requests from forks are run via Savage."
+fi
