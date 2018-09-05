1d9c1ef66d7181054671f96113e3a91c0c5e1e6a
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-09-05 17:06:09 -0400

    Altered build command

--- v3/library-after-success.sh
+++ v3/library-after-success.sh
@@ -3,7 +3,7 @@ set -e
 
 # Necessary to stop pull requests from forks from running.
 if [[ "$TRAVIS_SECURE_ENV_VARS" == "true" && -n "$TRAVIS_TAG" ]]; then
-  skyux build-public-library
+  npm run build
   cd dist
   bash <(curl -s https://blackbaud.github.io/skyux-travis/after-success.sh)
   node ./node_modules/@blackbaud/skyux-builder-config/scripts/visual-baselines.js
