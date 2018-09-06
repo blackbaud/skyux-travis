769258eb79f4db48028d43e4b2cd2fb1ecc2b091
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-09-06 13:51:29 -0400

    Added comment for npm install

--- v3/library-before-install.sh
+++ v3/library-before-install.sh
@@ -3,8 +3,14 @@ set -e
 
 # Necessary to stop pull requests from forks from running.
 if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
-  nvm install-latest-npm
+
+  # NPM versions less than v6 overwrite modules with subsequent installs.
+  # (NPM version 6.0.1 will work nicely with our version of Node.js.)
+  # See: https://github.com/npm/npm/issues/17379
+  npm install -g npm@6.0.1
+
   npm install -g @blackbaud/skyux-cli
+  skyux version
 else
   echo -e "Ignoring script. Pull requests from forks are run elsewhere."
 fi
