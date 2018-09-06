5a2250cb735c54dcf61c4a2aaf97349e4e11646e
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-09-06 08:13:29 -0400

    Remove visual command

--- v3/library-script.sh
+++ v3/library-script.sh
@@ -4,7 +4,6 @@ set -e
 # Necessary to stop pull requests from forks from running.
 if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
   npm run test:ci
-  npm run test:visual:ci
 else
   echo -e "Pull requests from forks are run via Savage."
 fi
