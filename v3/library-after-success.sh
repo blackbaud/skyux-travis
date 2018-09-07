0b0d40a5b1c1ca6ed9a212777ad0644c1b5e9234
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-09-07 13:24:23 -0400

    Added option to include custom build command

--- v3/library-after-success.sh
+++ v3/library-after-success.sh
@@ -3,10 +3,17 @@ set -e
 
 # Necessary to stop pull requests from forks from running.
 if [[ "$TRAVIS_SECURE_ENV_VARS" == "true" && -n "$TRAVIS_TAG" ]]; then
-  skyux build-public-library
+  # Allow package.json to specify a custom build script.
+  if npm run | grep -q build:ci; then
+    echo -e "Running custom build step... `npm run build:ci`";
+    npm run build:ci
+  else
+    skyux build-public-library
+  fi
+
   cd dist
   bash <(curl -s https://blackbaud.github.io/skyux-travis/after-success.sh)
   node ./node_modules/@blackbaud/skyux-builder-config/scripts/visual-baselines.js
 else
-  echo -e "Ignoring script. Releases are published during git tag builds only."
+  echo -e "Ignoring script. Releases are published after git tag builds only."
 fi
