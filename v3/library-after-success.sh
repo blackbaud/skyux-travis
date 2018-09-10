2f0976255bfed104de380f45fb2d36ad0a623933
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-09-10 11:10:48 -0400

    Fixed /v3/library-after-success to save new baselines

--- v3/library-after-success.sh
+++ v3/library-after-success.sh
@@ -2,18 +2,27 @@
 set -e
 
 # Necessary to stop pull requests from forks from running.
-if [[ "$TRAVIS_SECURE_ENV_VARS" == "true" && -n "$TRAVIS_TAG" ]]; then
-  # Allow package.json to specify a custom build script.
-  if npm run | grep -q build:ci; then
-    echo -e "Running custom build step... `npm run build:ci`";
-    npm run build:ci
-  else
-    skyux build-public-library
-  fi
+if [[ "$TRAVIS_SECURE_ENV_VARS" == "true" ]]; then
 
-  cd dist
-  bash <(curl -s https://blackbaud.github.io/skyux-travis/after-success.sh)
+  # Save any new baseline screenshots.
   node ./node_modules/@blackbaud/skyux-builder-config/scripts/visual-baselines.js
+
+  # Only run releases during a git tag build.
+  if [[ -n "$TRAVIS_TAG" ]]; then
+
+    # Allow package.json to specify a custom build script.
+    if npm run | grep -q build:ci; then
+      echo -e "Running custom build step... `npm run build:ci`";
+      npm run build:ci
+    else
+      skyux build-public-library
+    fi
+
+    cd dist
+    bash <(curl -s https://blackbaud.github.io/skyux-travis/after-success.sh)
+  else
+    echo -e "Aborting release. Releases are published after git tag builds only."
+  fi
 else
-  echo -e "Ignoring script. Releases are published after git tag builds only."
+  echo -e "Ignoring script. Pull requests from forks are run elsewhere."
 fi
