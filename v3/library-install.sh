50681e375bb7dd7fbc93a3a0869df8e4a859ece4
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2019-01-18 12:30:41 -0500

    Fix private npm installs

--- v3/library-install.sh
+++ v3/library-install.sh
@@ -4,8 +4,7 @@ set -e
 # Necessary to stop pull requests from forks from running.
 if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
   npm install
-  npm install --no-save blackbaud/skyux-builder-config
-  npm install --no-save blackbaud/skyux-sdk-builder-config
+  npm install --no-save blackbaud/skyux-builder-config blackbaud/skyux-sdk-builder-config
 else
   echo -e "Ignoring script. Pull requests from forks are run elsewhere."
 fi
