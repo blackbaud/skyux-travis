6caaa107ea08c7d8d5c1b5941038c3a1efdede7a
Steve Brush <steve.brush@blackbaud.com>
2019-04-04 10:30:45 -0400

    Install `@skyux-sdk/cli` (#12)

--- v3/library-before-install.sh
+++ v3/library-before-install.sh
@@ -3,13 +3,7 @@ set -e
 
 # Necessary to stop pull requests from forks from running.
 if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
-
-  # NPM versions less than v6 overwrite modules with subsequent installs.
-  # (NPM version 6.0.1 will work nicely with our version of Node.js.)
-  # See: https://github.com/npm/npm/issues/17379
-  npm install -g npm@6.0.1
-
-  npm install -g @blackbaud/skyux-cli
+  npm install -g @skyux-sdk/cli
   skyux version
 else
   echo -e "Ignoring script. Pull requests from forks are run elsewhere."
